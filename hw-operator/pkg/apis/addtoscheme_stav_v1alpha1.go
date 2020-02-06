0dd1dbc6a5128c15a420d2926b1a9fed6e32dedb
Stav Cohen <stavco9@gmail.com>
2020-02-06 08:37:21 +0000

    First Commit

---
+++ hw-operator/pkg/apis/addtoscheme_stav_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"github.com/stavco9/go-test/pkg/apis/stav/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
---
+++ hw-operator/pkg/apis/stav/group.go
@@ -0,0 +1,6 @@
+// Package stav contains stav API versions.
+//
+// This file ensures Go source parsers acknowledge the stav package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package stav
